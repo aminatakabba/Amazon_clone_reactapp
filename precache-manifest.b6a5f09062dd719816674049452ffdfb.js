7ae5a83dec5cbc2dea2f7be5b732a112df57f055
Aminatakabba <aminata-kabba@hotmail.com>
2020-09-22 12:35:35 +0100

    Updates

---
+++ precache-manifest.b6a5f09062dd719816674049452ffdfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2436bfe5dd554e62adb3ef4f275bf09f",
+    "url": "/Amazon_clone_reactapp/index.html"
+  },
+  {
+    "revision": "7c6f6a754ba88eeb253a",
+    "url": "/Amazon_clone_reactapp/static/css/main.19dc0618.chunk.css"
+  },
+  {
+    "revision": "bb59a8a83994f8dd5718",
+    "url": "/Amazon_clone_reactapp/static/js/2.e67883be.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/Amazon_clone_reactapp/static/js/2.e67883be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c6f6a754ba88eeb253a",
+    "url": "/Amazon_clone_reactapp/static/js/main.6881a8a3.chunk.js"
+  },
+  {
+    "revision": "bd250d435557cdb1b066",
+    "url": "/Amazon_clone_reactapp/static/js/runtime-main.4205b38a.js"
+  }
+]);
